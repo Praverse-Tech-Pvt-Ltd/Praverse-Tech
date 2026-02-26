262d942a4932933d12ea1241432550791b82b993
Maanav Shah <maanavshah151@gmail.com>
2026-02-26 17:30:15 +0530

    Updated the website icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,10 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="icon" href="/Logo%20Design%20for%20Praverse%20Tech%20Pvt.%20Ltd..png" />
-        <link rel="icon" href="/Logo%20Design%20for%20Praverse%20Tech%20Pvt.%20Ltd..png" sizes="192x192" />
-        <link rel="icon" href="/Logo%20Design%20for%20Praverse%20Tech%20Pvt.%20Ltd..png" sizes="512x512" />
-        <link rel="apple-touch-icon" href="/Logo%20Design%20for%20Praverse%20Tech%20Pvt.%20Ltd..png" />
+        <link rel="icon" href="/logo%20rotate.png" />
+        <link rel="icon" href="/logo%20rotate.png" sizes="192x192" />
+        <link rel="icon" href="/logo%20rotate.png" sizes="512x512" />
+        <link rel="apple-touch-icon" href="/logo%20rotate.png" />
         <link href="https://fonts.googleapis.com/css2?family=Satoshi:wght@400;500;700;900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
